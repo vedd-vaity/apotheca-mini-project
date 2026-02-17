c1325e10c196e5c73f264c7530234da1ea392e66
Vedd Vaity <vedd.vaity@somaiya.edu>
2026-02-17 15:23:26 +0530

    Update form-validation.js

--- form-validation.js
+++ form-validation.js
@@ -2,25 +2,25 @@ const form = document.querySelector("form");
 
 function formvalid(e) {
   e.preventDefault();
-
+  // adds all valid form input fields
   const name = form.querySelector('input[type="text"]');
   const email = form.querySelector('input[type="email"]');
   const password = form.querySelector('input[type="password"]');
   const roles = form.querySelectorAll('input[name="role"]');
   const departments = form.querySelectorAll('input[type="checkbox"]');
 
-  const emailPattern = /^[^@]+@[^@]+\.gov$/;
-
+  // tests if name is only alphabets and spaces
   if (!/^[A-Za-z ]+$/.test(name.value.trim())) {
     alert("Name must contain only letters and spaces");
     return;
   }
-
+  // tests if email is following an appropriate ppattern
+  const emailPattern = /^[^@]+@[^@]+\.gov$/;
   if (!emailPattern.test(email.value.trim())) {
     alert("email is wrong!");
     return;
   }
-
+  // deals with roles checking
   let rolePicked = false;
 
   for (let r of roles) {
@@ -34,6 +34,7 @@ function formvalid(e) {
     return;
   }
 
+  // deals with department checking
   let deptPicked = false;
   for (let d of departments) {
     if (d.checked) {
@@ -46,7 +47,7 @@ function formvalid(e) {
     alert("You havent picked a department!");
     return;
   }
-
+  // password check
   if (password.value.length < 8) {
     alert("password must be 8 characters or longer!");
     return;
